5f9100e99da827e34a1c1fdcd59d9b3b3b4086fb
Kaia Lee <kaialeedev@gmail.com>
2025-06-30 20:38:50 +0900

    JS-Work

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,32 @@
 'use strict';
+
+// Filter projects in the Work section based on category
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+categories.addEventListener('click', (e) => {
+  const filter = e.target.dataset.category;
+  if (filter == null) return;
+  handleActiveSelection(e.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  const active = categories.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
+}
+
+function filterProjects(filter) {
+  projects.forEach((project) => {
+    if (filter === 'all' || filter === project.dataset.type) {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  });
+  projectsContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projectsContainer.classList.remove('anim-out');
+  }, 250);
+}
